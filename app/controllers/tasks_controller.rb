72e6e3e83d3b0dd7c73eaeaefa5b42366277e22a
Haropon <kasyun0608@gmail.com>
2018-05-10 03:31:05 +0000

    Project Success

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'タスクを保存しました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクを保存できませんでした'
+      render :new
+    end
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = 'タスクを更新しました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクを更新できませんでした'
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:success] = 'タスクを削除しました'
+    redirect_to tasks_url
+  end
+  
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
